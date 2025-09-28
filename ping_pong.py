4acadcc61239bcdef8b5688aea8d7c9484a6fdce
delta55590 <kizilovaleksej19@gmail.com>
2025-09-28 13:48:39 +0700

    Update ping_pong.py
    есть сцена и управление спрайтами

--- ping_pong.py
+++ ping_pong.py
@@ -37,15 +37,42 @@ window = display.set_mode((win_width, win_height))
 display.set_caption("Пинг-понг")
 window.fill(back)
 
+ball = GameSprite("tennis.png", 250, 200, 50, 50, 5)
+raket1 = Player("platform.png", 30, 200, 20, 150, 5)
+raket2 = Player("platform.png", 520, 200, 20, 150, 5)
+
+
 game = True
 clock = time.Clock()
+finish = False
+
+speed_x = 2
+speed_y = 2
 
 while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
+    if finish != True:
+        window.fill(back)
+        raket1.update_1()
+        raket2.update_2()
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+
+        if ball.rect.y > win_height -50 or ball.rect.y < 0:
+            speed_y *= -1
+
+        if sprite.collide_rect(raket1, ball) or sprite.collide_rect(raket2, ball):
+            speed_x *= -1
+
+        ball.draw()
+        raket1.draw()
+        raket2.draw()
 
     display.update()
     clock.tick(60)
+
+   
 
    
